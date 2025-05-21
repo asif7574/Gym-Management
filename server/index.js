d9965c62acff70ced8b2127d7c77eef9992c73b5
N ASIFKHAN <asifkhan4092@gmail.com>
2025-05-21 18:51:10 +0530

    cors update

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app=express();
 app.use(express.json())
 app.use(
   cors({
-      origin: ["http://localhost:5173","https://gym-management-khaki-ten.vercel.app"] ,
+      origin: ["http://localhost:5173","https://gym-management-client-alpha.vercel.app"] ,
       credentials: true,
       methods: ["GET","POST","PUT","DELETE"]
   })
